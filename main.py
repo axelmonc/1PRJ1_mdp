825a97bbbf1ab60d3becc9d4d295914aa81ecbd8
axelmonc <110297@ecole-it.com>
2026-01-15 09:43:32 +0100

    ajout des fonctions et ce qu elles font dans main.py

---
+++ main.py
@@ -0,0 +1,54 @@
+from random import randint
+
+def generer_mdp():
+    """
+    fonction qui génére une chaîne aléatoire ou prononçable et retourner le mot de passe.
+    :return:
+    """
+    return None
+def analyser_force():
+    """
+    fonction qui calcule l'entropie et verifie les critères
+    :return: int, le score (compris entre 0 et 100)
+    """
+    return None
+def ajouter_compte():
+    """
+    fonction qui ajoute un compte
+    :return:
+
+    """
+
+    return None
+def lister_comptes():
+
+    """
+    fonction qui liste les comptes
+    :return:
+    """
+    return None
+def rechercher():
+    """
+    fonction qui trouve un compte par nom de site ou catégorie
+    :return:
+    """
+    return None
+def calculer_stats():
+    """
+    fontion qui affiche le nombre de comptes, la répartition par catégorie, le score moyen et les mots de passe à changer
+    :return:
+    """
+    return None
+def sauvegarder():
+    """
+    fonction qui convertit la liste de dictionnaires en format JSON.
+    :return:
+    """
+    return None
+
+voyelles_min = ["a","e","i","o","u","y"]
+consonnes_min = ["b","c","d","f","g","h","j","k","m","n","p","q","r","s","t","v","w","x","z"]
+voyelles_maj = ["A","E","U","Y"]
+consonnes_maj = ["B","C","D","F","G","H","J","K","L","M","N","P","Q","R","S","T","V","W","X","Z"]
+chiffres = ["2","3","4","5","6","7","8","9"]
+caracteres = ["_", "-", "@"]
